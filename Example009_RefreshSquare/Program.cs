60421fd1c1ef93a66b5dfdc8c5115b0261729651
RuslanErkenov <bad.hunter.09@mail.ru>
2022-10-13 21:23:07 +0300

    Example009_RefreshSquare

---
+++ Example009_RefreshSquare/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter a ");
+string a = Console.ReadLine();
+
+Console.WriteLine("Enter b ");
+string b = Console.ReadLine();
+
+if(a == b*b)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
