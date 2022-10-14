c53bb455a72d415806c8ee6132cc96f17fa30c75
RuslanErkenov <bad.hunter.09@mail.ru>
2022-10-14 21:05:35 +0300

    Sem02Ex02

---
+++ Sem02Ex02/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num = new Random().Next(100, 999);
+
+int result = ((number / 100) * 10) + number %10;
+
+Console.WriteLine(num);
+Console.WriteLine(result);
+
+System.Comsole.WriteLine($"Was {number}, Now {result}"); // ("Was " + number + "..." + result)
