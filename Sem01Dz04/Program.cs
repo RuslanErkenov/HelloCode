5e315982202f968775350e818feacdc1ddc17161
RuslanErkenov <bad.hunter.09@mail.ru>
2022-10-14 13:30:35 +0300

    Sem01Dz04

---
+++ Sem01Dz04/Program.cs
@@ -0,0 +1,13 @@
+﻿// На входе число, а на выходе все четные от 1 до числа на входе
+//Console.Write("Enter number: ");
+//int num = int.Parse(Console.ReadLine());
+Console.Write("Write number 1: ");
+int n = int.Parse(Console.ReadLine());
+for (int number = 1; number < 21; number++)
+{
+    if (number % 3 == 0)
+        {
+            sum = sum + number;
+        }
+    }
+    Console.WriteLine($"The sum is {sum}");
