98c708e330950caa296ed12ff2183c0a7c29b18b
RuslanErkenov <bad.hunter.09@mail.ru>
2022-10-14 21:22:53 +0300

    Sem02Ex03

---
+++ Sem02Ex03/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("First number: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Second number: ");
+int b = int.Parse(Console.ReadLine());
+int result = a % b;
+if (result == 0){
+    Console.WriteLine("Успешно разделили a на b ");
+}
+else
+{
+    Console.WriteLine(result);
+}
+
+
