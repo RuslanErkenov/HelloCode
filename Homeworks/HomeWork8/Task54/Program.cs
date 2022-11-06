eb9d7a27dec962d94805ec2fb2a368347856f93a
RuslanErkenov <bad.hunter.09@mail.ru>
2022-11-06 18:23:22 +0300

    HomeTask8

--- Homeworks/HomeWork8/Task54/Program.cs
+++ Homeworks/HomeWork8/Task54/Program.cs
@@ -1,13 +1,13 @@
-﻿// Задайте двумерный массив. Напишите программу, которая упорядочит 
-// по убыванию элементы каждой строки двумерного массива.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// В итоге получается вот такой массив:
-// 7 4 2 1
-// 9 5 3 2
-// 8 4 4 2
+﻿/*Задайте двумерный массив. Напишите программу, которая упорядочит 
+по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
 
 Console.WriteLine("enter m: ");
 int m = int.Parse(Console.ReadLine());
@@ -16,10 +16,9 @@ int n = int.Parse(Console.ReadLine());
 
 int[,] array = new int[m, n];
 
-int[,] GetArray(int m, int n)
+int[,] GetArray(int[,] array)
 
 {
-    int[,] array = new int[m, n];
     Random rnd = new Random();
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -46,21 +45,21 @@ void PrintArray(int[,] array)
 
 
 
-int SortSting(int[,] array)
+void SortString(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {  
         
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+            for (int z = 0; z < array.GetLength(1) - 1; z++)
             {        
-                if (numbers[i, z] < numbers[i, z + 1]) //для изменения сортировки поменять знак < на противоположный
+                if (array[i, z] < array[i, z + 1]) //для изменения сортировки поменять знак < на противоположный
                     {
                     int temp = 0;
-                    temp = numbers[i, z];
-                    numbers[i, z] = numbers[i, z + 1];
-                    numbers[i, z + 1] = temp;
+                    temp = array[i, z];
+                    array[i, z] = array[i, z + 1];
+                    array[i, z + 1] = temp;
                     }          
             }
         } 
@@ -73,4 +72,5 @@ Console.WriteLine("Массив до изменения");
 PrintArray(array);
 Console.WriteLine();
 Console.WriteLine("Массив с упорядоченными значениями");
-SortSting(array);
+SortString(array);
+PrintArray(array);
